8f74b57526e9b7fd6a5760de8704a559968a7741
hmzunal <hamza.unalim@gmail.com>
2022-12-07 19:29:01 +0300

    css ekleme denemeleri

---
+++ src/components/C1.jsx
@@ -0,0 +1,26 @@
+import './style.css'
+
+// const paragrafStili={
+
+//     color:"red",
+//     backgroundColor:"black"
+
+
+// }
+
+function C1()
+{
+        return(
+            // style={paragrafStili}
+                <>
+                        <p className='ozel-paragraf'>Lorem deneme yazıları. Lorem deneme yazıları.  Lorem deneme yazıları.  Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. Lorem deneme yazıları. </p>                
+
+
+                       
+                </>
+
+        )
+}
+
+
+export default C1;
